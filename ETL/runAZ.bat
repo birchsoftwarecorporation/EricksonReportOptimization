2a242a0e82676dfbb53f8a8c1de62fff3a229dfd
Birch Software <sean@birchsoftware.com>
2015-07-08 23:20:28 -0700

    Pan script to run ETL

---
+++ ETL/runAZ.bat
@@ -0,0 +1,4 @@
+rem http://wiki.pentaho.com/display/EAI/Kitchen+User+Documentation
+rem http://infocenter.pentaho.com/help/index.jsp?topic=%2Fpdi_user_guide%2Freference_pan.html
+
+C:\pentaho\pentaho-data-integration\Pan.bat -file="C:\BirchSoftware\github\EricksonReportOptimization\ETL\Extract Job Card Data.ktr" -logfile=C:\BirchSoftware\github\EricksonReportOptimization\ETL\jobcard-output.log -level=Basic -param:KETTLE_TRANS_ROWSET_SIZE=1000000
